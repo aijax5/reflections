594a067ac7c58d15ba5c63e433734d44fa1abb79
anupamchanda2011 <anupamchanda2011@gmail.com>
2020-09-10 16:40:53 +0530

    refactoring done

--- blog/views/home.py
+++ blog/views/home.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
-from blog.models.post import Post
+from blog.models.post import Post,Tag
 
 NUM_OF_POSTS = 5
 
@@ -14,16 +14,20 @@ def home(request, username=None):
         user = User.objects.get(username=username)
         first_name = user.first_name
         last_name = user.last_name
-        post_list = Post.objects.filter(user=user)
+        # post_list = Post.objects.filter(user=user)
+        username = "tag1"
+        b1 = Tag.objects.get(name = username)
+        posts = Post.objects.filter(tag=b1)
     else:
-        post_list = Post.objects.all()
+        # post_list = Post.objects.all()
+        posts = Post.objects.filter()
 
-    post_list = post_list.order_by('-pub_date')
+    # post_list = post_list.order_by('-pub_date')
 
-    paginator = Paginator(post_list, NUM_OF_POSTS)  # Show NUM_OF_PAGES posts per page
-    page = request.GET.get('page')
+    # paginator = Paginator(post_list, NUM_OF_POSTS)  # Show NUM_OF_PAGES posts per page
+    # page = request.GET.get('page')
 
-    posts = paginator.get_page(page)
+    # posts = paginator.get_page(page)
 
     return render(request, 'blog/home.html', {'posts': posts,
                                               'first_name': first_name,
